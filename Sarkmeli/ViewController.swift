37eff9ccc2e9e1b909567752c207841ad7f4e139
Salome <salomemsxvilidze@icloud.com>
2025-06-28 11:27:21 +0400

    first commit

---
+++ Sarkmeli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sarkmeli
+//
+//  Created by Mcbook Pro on 28.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
